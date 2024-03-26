52249fc557220ba72b30a8b500296b764f8ba4e7
Gwladys <134710830+Gwladys75@users.noreply.github.com>
2024-03-26 12:35:10 +0100

    Add files via upload

---
+++ exercices_php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercices PHP</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+<body>
+
+    <?php
+
+        $bleu = 'Bleu-';
+        $vert = 'Vert-';
+        $rouge= 'Rouge';
+
+    echo "<p><span class=\"bleu\">$bleu</span><span class=\"vert\">$vert</span><span class=\"rouge\">$rouge</span></p>";
+
+    ?>
+
+</body>
+</html>
+
